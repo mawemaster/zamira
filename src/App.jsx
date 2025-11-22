66df52ae54fa5041f412ad8bc9ccea622f110aec
mawemaster <mawemaster@gmail.com>
2025-11-22 00:34:01 -0300

    bug 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import './App.css'
 import Pages from "@/pages/index.jsx"
 import { Toaster } from "@/components/ui/toaster"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+
+// Criando o "motor" de dados
+const queryClient = new QueryClient()
 
 function App() {
   return (
-    <>
+    // Envolvendo o site inteiro com o provedor de dados
+    <QueryClientProvider client={queryClient}>
       <Pages />
       <Toaster />
-    </>
+    </QueryClientProvider>
   )
 }
 
-export default App 
+export default App
