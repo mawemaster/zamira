d8ea62ed1643a35227a00bc0b2d35e4879cf48f3
mawemaster <mawemaster@gmail.com>
2025-11-22 12:46:42 -0300

    novo

--- src/api/supabaseClient.js
+++ src/api/supabaseClient.js
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Usa as chaves que você configurou ou as hardcoded como fallback
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || "https://pjzviwiusmwhaszkonmr.supabase.co"
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBqenZpd2l1c213aGFzemtvbm1yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM3Nzc0NDksImV4cCI6MjA3OTM1MzQ0OX0.CGBWvI96upc4jOyluyu-_eXEmBfXZH5WHg1PcVTEygE"
 
